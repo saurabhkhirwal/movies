890f4ec1a5cde0bf80459c95219f41bdd9641bfe
Saurabh Khirwal <saurabhkhirwal2010@gmail.com>
2020-08-11 23:53:52 +0530

    Fix

--- index.php
+++ index.php
@@ -3,13 +3,25 @@
 <head>
 	<title>Movie Manager</title>
 	<style type="text/css">
-		body, h3 {
+		body {
 			padding: 0;
 			margin: 0;
-			font-family: sans-serif;
+			font-family: "Consolas", sans-serif;
+			background: #adadad;
 		}
-		button:hover {
+		h3 {
+			padding: 0;
+			margin: 0;
+		}
+		button {
 			cursor: pointer;
+			font-family: "Consolas", sans-serif;
+		}
+		#wrapper {
+			margin: 0px auto;
+			padding: 15px; 
+			max-width: 900px;
+			background: #ffffff;
 		}
 		#myMovies {
 			padding-top: 20px;
@@ -26,12 +38,17 @@
 		}
 		.movie {
 			width: 200px;
-			height: 300px;
 			text-align: center;
 			float: left;
 			margin-right: 30px;
 			margin-bottom: 30px;
 			font-size: 14px;
+			background: #dedede;
+			padding: 10px;
+			border-radius: 5px;
+		}
+		.movie:hover {
+			background: #9d9d9d;
 		}
 		.movie img {
 			max-width: 100%;
@@ -54,6 +71,15 @@
 			margin: 150px auto;
 			padding: 20px;
 		}
+		/*#subscription .sucscribed {
+			display: none;
+		}
+		#subscription.true .sucscribed {
+			display: block;
+		}
+		#subscription.true .unsucscribed {
+			display: none;
+		}*/
 	</style>
 </head>
 <body>
@@ -64,8 +90,17 @@
 			<button class="popup_button" onclick="addMovie();" >Add Movie</button>
 		</div>
 	</div>
-	<button class="popup_button" onclick="$('#popup_wrapper').show();" >Add Movie</button>
-	<div id="myMovies" ></div>
+
+	<div id="wrapper" >
+		<!-- 
+			<div id="subscription" onclick="$('#subscription').toggleClass('true');" >
+				<p class="unsucscribed" >Subscribe</p><p class="sucscribed" >UnSubscribe | <b>Bell</b></p>
+			</div>
+		-->
+		<button class="popup_button" onclick="$('#popup_wrapper').show();" >Add Movie</button>
+		<div id="myMovies" ></div>
+		<div style="clear:both;" ></div>
+	</div>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script type="text/javascript">
 		$(() => {
